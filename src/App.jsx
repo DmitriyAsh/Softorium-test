1dde86f0f42ac2ce868e25ea191d274dad8c1638
Dmitriy Dmitriev <dmitriyd1107@gmail.com>
2024-03-13 20:19:52 +0700

    finish head of first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<div className='head'>
+				<p className='first__string'>Хочешь работать над</p>
+				<p className='second__string'>интересными проектами?</p>
+			</div>
+			<div className='line'></div>
+			<div className='team__requirements'>
+				в команду Softorium требуются:
+			</div>
+		</div>
+	);
 }
 
 export default App;
