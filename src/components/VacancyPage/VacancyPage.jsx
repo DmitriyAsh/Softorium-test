7baa7f68f048e00388f314534efd7a9230377bb8
Dmitriy Dmitriev <dmitriyd1107@gmail.com>
2024-03-14 17:04:05 +0700

    start making second page

--- src/components/VacancyPage/VacancyPage.jsx
+++ src/components/VacancyPage/VacancyPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import cl from './VacancyPage.module.css';
 
 const VacancyPage = () => {
 	const [vacancyInfo, setVacancyInfo] = useState([]);
@@ -27,7 +28,26 @@ const VacancyPage = () => {
 		fetchVacancyInfo();
 	}, []);
 
-	return <div></div>;
+	return (
+		<div>
+			<div className={cl.head}>
+				<p className={cl.first__string}>Ищем {vacancyInfo.name}'а</p>
+				<p className={cl.second__string}>
+					Трудоустройство в аккредитованную ИТ-компанию
+				</p>
+			</div>
+			<div className={cl.about_block}>
+				<p className={cl.about_text}>
+					Мы любим сложные, интересные проекты, постоянно расширяемся
+					и именно поэтому ищем новые таланты. В коллективе Softorium
+					вы найдете возможность воплотить идеи любого уровня
+					нестандартности и вместе с тем перенять опыт наших ведущих
+					разработчиков.
+				</p>
+				<div className={cl.about_image}></div>
+			</div>
+		</div>
+	);
 };
 
 export default VacancyPage;
